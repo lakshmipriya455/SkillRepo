7ebde1f09f5f3da97b06d88bdbb8ca941722ee4e
lakshmipriya455 <lakshmipriya455@gmail.com>
2018-10-25 16:42:28 +0530

    middleware added

---
+++ spring-tarangini-restful/src/main/java/com/tara/SpringTaranginiRestfulApplication.java
@@ -0,0 +1,12 @@
+package com.tara;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTaranginiRestfulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringTaranginiRestfulApplication.class, args);
+	}
+}
---
+++ spring-tarangini-restful/src/test/java/com/tara/SpringTaranginiRestfulApplicationTests.java
@@ -0,0 +1,16 @@
+package com.tara;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringTaranginiRestfulApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
